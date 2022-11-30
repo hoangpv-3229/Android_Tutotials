f6eafc4e5e118c7218fea2dce8d758640d404733
hoangpv-3229 <phung.van.hoang@sun-asterisk.com>
2022-11-30 16:07:49 +0700

    add tut_5: implicit intent

---
+++ tut_5_impicit_intents/src/main/java/vn/ztech/software/tut_5_impicit_intents/MainActivity.kt
@@ -0,0 +1,59 @@
+package vn.ztech.software.tut_5_impicit_intents
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import vn.ztech.software.tut_5_impicit_intents.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setViews()
+    }
+
+    private fun setViews() {
+        binding.btWeb.setOnClickListener {
+            openWebsite(binding.etWeb.text.toString())
+        }
+        binding.btLocation.setOnClickListener {
+            openLocation(binding.etLocation.text.toString())
+        }
+        binding.btShare.setOnClickListener {
+            shareText(binding.etShare.text.toString())
+        }
+    }
+
+    private fun openWebsite(text: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(text))
+        startAct(intent, "Wrong website format")
+    }
+
+    private fun openLocation(text: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getUriTextFromAddress(text)))
+        startAct(intent, "Wrong location format")
+    }
+
+    private inline fun getUriTextFromAddress(text: String): String = "geo:0,0?q=${Uri.encode(text)}"
+    /**any string must be encoded in the uri passed for GG map intents*/
+
+    private fun shareText(text: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        startAct(intent, "Wrong text format")
+    }
+
+    private fun startAct(intent: Intent, errorString: String) {
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, errorString, Toast.LENGTH_LONG).show()
+        }
+    }
+}
